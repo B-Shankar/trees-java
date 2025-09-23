9c60cd7e05ca79b25b96ff105a94fb4e03ba702a
B-Shankar <learn.bhimashankar@gmail.com>
2025-09-23 23:44:06 +0530

    In-Order Traversal

--- src/trees/TraversalBT.java
+++ src/trees/TraversalBT.java
@@ -10,7 +10,7 @@ public class TraversalBT {
         this.root = root;
     }
 
-    // Pre-Order Traversal: Root -> Left -> Right
+    // 1. Pre-Order Traversal: Root -> Left -> Right
     public void preOrder() {
         preOrder(root);
     }
@@ -24,18 +24,37 @@ public class TraversalBT {
         preOrder(node.right);
     }
 
+    // 2. In-Order Traversal: Left -> Root -> Right
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(BinaryTrees.Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BinaryTrees binaryTrees = new BinaryTrees();
         binaryTrees.populate(scanner);
 
         // Prettier display
-        binaryTrees.prettierDisplay();
+        //binaryTrees.prettierDisplay();
 
         // Traversal
         TraversalBT traversal = new TraversalBT(binaryTrees.getRoot());
         System.out.print("Pre-order Traversal: ");
         traversal.preOrder();
+
+        System.out.println();
+
+        System.out.print("In-order Traversal: ");
+        traversal.inOrder();
 
         scanner.close();
     }
