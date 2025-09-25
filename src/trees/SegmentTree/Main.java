7a1943e44cdfbccae1c1ec59790c917718cf6343
B-Shankar <learn.bhimashankar@gmail.com>
2025-09-25 23:30:55 +0530

    example

--- src/trees/SegmentTree/Main.java
+++ src/trees/SegmentTree/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {3, 8, 7, 6, -2, -8, 4, 9};
         SegmentTree segmentTree = new SegmentTree(arr);
-        segmentTree.display();
+
+        //segmentTree.display();
+
+        System.out.println(segmentTree.query(1, 6)); //15 = [8 + 7 + 6 - 2 - 8 + 4]
     }
 }
